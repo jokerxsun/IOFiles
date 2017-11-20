2d80b6451b0c17ce5e96c01768597b6f0b487444
孙旭 <sun1003103044@163.com>
2017-11-20 20:38:37 +0800

    之前仓促实现功能，现在使用递归方式实现只要在源文件夹下，所有文件都可以进行合并，解决文件夹内文件复制粘贴的痛点

--- IOFiles/IOFiles/ViewController.m
+++ IOFiles/IOFiles/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (nonatomic, assign) NSInteger count;
+@property (nonatomic, strong) NSMutableArray *allFiles;
+
 @end
 
 @implementation ViewController
@@ -17,7 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // 获取沙盒文件路径
+    // 获取沙盒路径
     NSString *documentPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
     
     // 创建目标文件路径
@@ -26,37 +29,54 @@
     // 创建文件管理器
     NSFileManager *fileManager = [NSFileManager defaultManager];
     
-    // 获取沙盒中文件列表
-    NSArray *documentFileList = [[NSArray alloc] init];
-    
     NSError *error;
     
-    documentFileList = [fileManager contentsOfDirectoryAtPath:documentPath error:&error];
+    // 目录下所有文件
+    NSArray *documentFileList = [self p_allFilesAtPath:documentPath];
     
-    // 读取文件中内容，写入一个文件中，归档
-    NSString *resultString = [[NSString alloc] init];
+    if (documentFileList.count == 0) {
+        return;
+    }
     
-    for (NSInteger i = 0; i < documentFileList.count; i++ ) {
+    // 读取内容
+    NSString *resultString = nil;
+    
+    for (NSInteger i = 0; i < documentFileList.count; i++) {
         
-        NSString *orginFilePath = [documentPath stringByAppendingPathComponent:documentFileList[i] ];
+        NSString *orginFilePath = documentFileList[i];
         
         NSString *readString = [NSString stringWithContentsOfFile:orginFilePath encoding:NSUTF8StringEncoding error:&error];
         
         resultString = [resultString stringByAppendingFormat:@"\n%@",readString];
     }
+    // 根据文件格式特定要求进行截取
     resultString = [resultString substringFromIndex:8];
     
     // 创建目标文件
     [fileManager createFileAtPath:resultFilePath contents:[resultString dataUsingEncoding:NSUTF8StringEncoding] attributes:nil];
     
-    NSLog(@"%@", resultFilePath);
-    
-    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"目标文件路径：\n%@", [documentPath stringByAppendingPathComponent:resultFilePath]);
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSArray *)p_allFilesAtPath:(NSString *)documentPath
+{
+    NSFileManager *fm = [NSFileManager defaultManager];
+    
+    // 是否是文件夹
+    BOOL isDir = NO;
+    BOOL isExist = [fm fileExistsAtPath:documentPath isDirectory:&isDir];
+    if (!isExist) return @[];
+    if (isDir) {
+        NSArray *subFiles = [fm contentsOfDirectoryAtPath:documentPath error:nil];
+        for (NSString *subPath in subFiles) {
+            /// 递归获取文件夹下面的文件， 直到找到所有的文件
+            [self p_allFilesAtPath:[documentPath stringByAppendingPathComponent:subPath]];
+        }
+    } else {
+        [_allFiles addObject:documentPath];
+    }
+    
+    return _allFiles;
 }
 
 @end
